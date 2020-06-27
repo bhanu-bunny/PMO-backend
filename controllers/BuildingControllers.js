eac97e929eee56edfce21ecf53b95fa3f3b32c56
Bunny <bhanueppakayala@gmail.com>
2020-06-27 18:26:11 +0530

    sat commit

--- controllers/BuildingControllers.js
+++ controllers/BuildingControllers.js
@@ -2,30 +2,24 @@ const express = require('express');
 var router = express.Router();
 var ObjectId = require('mongoose').Types.ObjectId;
 
-var {Projectname ,Entities,SuperLocations} = require('../models/Building');
+var {Entities,SuperLocations} = require('../models/Building');
 
 
-router.post('/Projects/',(req, res) => {
-    var Project= new Projectname({
-        name: req.body.name
-    });
-
-    Project.save((err, doc) => {
-        if (!err) { res.send(doc); }
-        else { console.log('Error in adding project name :' + JSON.stringify(err, undefined, 2)); }
-    });
-});
 
 
 router.post('/Entities/',(req, res) => {
   var entity = new Entities({
-    name:req.body.name
-  });
+
+    Project: req.body.Project,
+    EntityName:req.body.EntityName,
+    Entitycode: req.body.Entitycode
+  })
 
   entity.save((err,doc)=>{
     if (!err) { res.send(doc); }
         else { console.log('Error in adding entity name :' + JSON.stringify(err, undefined, 2)); }
-  })
+  }
+  )
 
 
 
